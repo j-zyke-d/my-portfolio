2ea6734647bfe1ef353431ab3ea38dcb64cfa798
j-zyke-d <dolletejj25@gmail.com>
2025-08-25 22:50:05 +0800

    configure the tailwind.config.ts: add fonts and project's color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,22 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: "class", // enables dark mode toggle (using a "dark" class)
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-montserrat)", "sans-serif"], // Montserrat
+        mono: ["var(--font-roboto-mono)", "monospace"], // Roboto Mono
+      },
       colors: {
-        primary: "#2563eb", // example blue (you can change later)
-        secondary: "#9333ea", // example purple
+        primary: "#181C14", // Dark Green-Black
+        secondary: "#3C3D37", // Gray-Green
+        accent: "#697565", // Muted Olive
+        background: "#ECDFCC", // Soft Beige
       },
     },
   },
