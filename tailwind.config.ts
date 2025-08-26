01af1b78a368b4993fb283a6b226b361a38604cc
j-zyke-d <dolletejj25@gmail.com>
2025-08-26 18:40:47 +0800

    update dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,10 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-montserrat)", "sans-serif"], // Montserrat
-        mono: ["var(--font-roboto-mono)", "monospace"], // Roboto Mono
-      },
-      colors: {
-        primary: "#181C14", // Dark Green-Black
-        secondary: "#3C3D37", // Gray-Green
-        accent: "#697565", // Muted Olive
-        background: "#ECDFCC", // Soft Beige
-      },
-    },
-  },
   plugins: [],
 };
 
